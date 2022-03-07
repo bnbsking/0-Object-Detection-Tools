db307ae4e5a6851ffb9d6c65b208ce70f816e34a
Bobo <bnbsking@gmail.com>
2022-03-07 11:35:06 +0800

    Create newNMS.py

---
+++ newNMS.py
@@ -0,0 +1,15 @@
+def NMS(bboxes, boxesType="yoloFloat", threshold=0.3): # bboxes: np.array
+    alive, adopt = set(range(len(bboxes))), []
+    while len(alive)>=2:
+        ma = min(alive)
+        adopt.append( ma )
+        boxA = getattr(boxAny2Voc,boxesType)(bboxes[ma][0], bboxes[ma][1], bboxes[ma][2], bboxes[ma][3], width=1000, height=1000)
+        alive.remove( ma )
+        for idx in alive.copy():
+            boxB = getattr(boxAny2Voc,boxesType)(bboxes[idx][0], bboxes[idx][1], bboxes[idx][2], bboxes[idx][3], width=1000, height=1000)
+            iou  = IOU(boxA,boxB)
+            if iou>=threshold:
+                alive.remove(idx)
+    if len(alive)==1:
+        adopt.append(alive.pop())
+    return adopt
